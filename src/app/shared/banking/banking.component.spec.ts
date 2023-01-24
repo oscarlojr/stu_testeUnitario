e4d2fac2e396d7cdca5646da3418cb268b179d02
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2023-01-23 21:04:37 -0300

    Criando teste para os gets e sets

--- src/app/shared/banking/banking.component.spec.ts
+++ src/app/shared/banking/banking.component.spec.ts
@@ -20,4 +20,28 @@ describe('BankingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('(U) getPoupanca(): Valida valor em conta', ()=> {
+    expect(component.getPoupanca).toEqual(10);
+  });
+
+  it('(U) getCarteira(): Valida valor na carteira', () => {
+    expect(component.getCarteira).toEqual(50);
+  }); 
+
+  it('(U) setSacar(): Deve transferir de popança para carteira',() => {
+    component.setSacar('10');
+    fixture.detectChanges();
+    expect(component.getPoupanca).toEqual(0);
+    expect(component.getCarteira).toEqual(60);
+  });
+
+  it('(U) setDepositar(): Deve transferir de carteira para poupança', () => {
+    component.setDepositar('20');
+    fixture.detectChanges();
+    expect(component.getCarteira).toEqual(30);
+    expect(component.getPoupanca).toEqual(30);
+  });
+
 });
+
