d101506e3034b3e8204c2734307caa9e1253e627
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2023-01-27 19:21:35 -0300

    Criando component e validando declaração no componente principal

---
+++ src/app/shared/investiments/list/list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+// Model
+import { Investiments } from '../model/investiments';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+
+  public investiments:Array<Investiments> =[
+    {
+      name: "Itaú",
+      value: 100
+    },
+    {
+      name: "Banco de Brasil",
+      value: 254
+    },
+    {
+      name: "Nubank",
+      value: 260
+    },
+    {
+      name: "Bradesco",
+      value: 369
+    },
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
