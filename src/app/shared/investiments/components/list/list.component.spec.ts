f59bdf201e5a4719bb922026d1dea1f976957524
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2023-02-09 10:20:11 -0300

    Criando teste unitário de interface

--- src/app/shared/investiments/components/list/list.component.spec.ts
+++ src/app/shared/investiments/components/list/list.component.spec.ts
@@ -27,4 +27,11 @@ describe('ListComponent', () => {
     expect(investiments[0].name).toContain('Itaú');
     expect(investiments[3].name).toContain('Bradesco');
   });
+  it('(I) Should list investiment', () => {
+    let investiment = fixture.debugElement.nativeElement.querySelectorAll('.list_items');
+
+    expect(investiment.length).toBe(4);
+    expect(investiment[0].textContent.trim()).toEqual('Itaú | 100');
+    expect(investiment[3].textContent.trim()).toEqual('Bradesco | 369');
+  });
 });
